9566b7d956efa319f8f3d0fc61ada9b32a1f55c1
dev-engr-spade <dev.engr.spade@gmail.com>
2025-03-09 22:13:57 +0800

    front end test

--- synchro-command/parent/parent-frontend/command-front/src/App.js
+++ synchro-command/parent/parent-frontend/command-front/src/App.js
@@ -45,11 +45,11 @@ const App = () => {
         }
       >
         <Routes>
-          <Route exact path="/login" name="Login Page" element={<Login />} />
+          <Route exact path="*" name="Login Page" element={<Login />} />
           <Route exact path="/register" name="Register Page" element={<Register />} />
           <Route exact path="/404" name="Page 404" element={<Page404 />} />
           <Route exact path="/500" name="Page 500" element={<Page500 />} />
-          <Route path="*" name="Home" element={<DefaultLayout />} />
+          <Route exact path="/dashboard" name="Home" element={<DefaultLayout />} />
         </Routes>
       </Suspense>
     </HashRouter>
